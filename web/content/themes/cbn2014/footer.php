8de9638f4b4bb6faa6bf66915930da3179fee744
jeremyBass <jeremybass@cableone.net>
2014-03-06 11:27:28 -0800

    removes the solical links in footer

--- web/content/themes/cbn2014/footer.php
+++ web/content/themes/cbn2014/footer.php
@@ -18,7 +18,7 @@
     <footer id="footer">
 
             <div id="footer-bkgrd">
-        <div id="social-icons">
+        <!--<div id="social-icons">
             <ul>
                 <li id="Twitter"><a href="https://twitter.com/WSUCougarPride" title="Twitter">Twitter</a></li>
                 <li id="Facebook"><a href="https://www.facebook.com/WSUAA" title="Facebook">Facebook</a></li>
@@ -26,14 +26,15 @@
                 <li id="LinkedIn"><a href="http://www.linkedin.com/groups?gid=35258" title="LinkedIn">LinkedIn</a></li>
                 <li id="Join-WSUAA"><a href="https://secure.alumni.wsu.edu/olc/pub/WHG/membershipform/showGivingForm.jsp?form_id=117412" title="Join WSUAA">Join WSUAA</a></li>
             </ul>
-        </div><!--#social-icons-->
+        </div>#social-icons-->
 			
 			
-			
-                <a href="http://alumni.wsu.edu/">Washington State University Alumni Association</a> | <a href="http://wsu.edu/">Washington State University</a>, PO Box 646150, Pullman, WA 99164-6150 | <a href="http://alumni.wsu.edu/olc/pub/WHG/volunteer/page_volunteer_1.jsp">Contact Us</a><br>
-                <span class="small-text">
-                    Copyright &copy; 2012 Board of Regents, Washington State University | <a href="http://access.wsu.edu/">Accessibility</a> | <a href="http://policies.wsu.edu/">Policies</a> | <a href="http://publishing.wsu.edu/copyright/WSU.html">Copyright</a> 
-                </span><!--.small-text-->
+				<span id="contact_block">
+					<a href="http://alumni.wsu.edu/">Washington State University Alumni Association</a> | <a href="http://wsu.edu/">Washington State University</a>, PO Box 646150, Pullman, WA 99164-6150 | <a href="http://alumni.wsu.edu/olc/pub/WHG/volunteer/page_volunteer_1.jsp">Contact Us</a><br>
+					<span class="small-text">
+						Copyright &copy; 2012 Board of Regents, Washington State University | <a href="http://access.wsu.edu/">Accessibility</a> | <a href="http://policies.wsu.edu/">Policies</a> | <a href="http://publishing.wsu.edu/copyright/WSU.html">Copyright</a> 
+					</span><!--.small-text-->
+				</span>
             </div><!--#footer-bkgrd-->
 
     </footer>   
