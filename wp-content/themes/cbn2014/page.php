60aa79231f9653f3df9cbb53f2b3964168681525
jeremyBass <jeremybass@cableone.net>
2014-04-06 22:09:39 -0700

    content change

--- wp-content/themes/cbn2014/page.php
+++ wp-content/themes/cbn2014/page.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 <div id="primary" class="content-area row">
-	<div id="content" class="site-content <?=(is_front_page()?"eight":"twelve")?> columns" role="main">
+	<div id="content" class="site-content <?=(is_front_page()?"eight":"twelve")?> twelve columns" role="main">
 		<?php /* The loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -29,13 +29,13 @@ get_header(); ?>
 		<?php endwhile; ?>
 	</div><!-- #content -->
 	<?php if ( is_front_page() ) { //NOTE this is to be replaced by the ad manager ?>
-		<aside id="adspace_home" class="four columns">
+		<!--<aside id="adspace_home" class="four columns">
 			<a href="https://www.applyonlinenow.com/USCCapp/Ctl/entry?sc=VABJ6K&mboxSession=1357690082163-219380"><img src="/wp-content/themes/cbn/img/sponsors/AlaskaAirlines.png" alt="Alaska Airlines Cougar Visa" title="Alaska Airlines Cougar Visa"></a>
 			<a href="http://www.ryanswansonlaw.com/"><img src="/wp-content/themes/cbn/img/sponsors/RyanSwanson.png" alt="Ryan, Swanson &amp; Cleveland, PLLC" title="Ryan, Swanson &amp; Cleveland, PLLC"></a>
 			<a href="http://www.griffinmaclean.com/"><img src="/wp-content/themes/cbn/img/sponsors/GriffinMacLean.jpg" alt="Griffin MacLean Insurance Brokers" title="Griffin MacLean Insurance Brokers"></a>
 			<a href="http://omba.wsu.edu/landingform_jun2011_b/?Access_Code=WSU-MBA-ALUMBANFEB2013&utm_source=ALUMBANFEB2013&utm_medium=internetadvertising"><img src="/wp-content/themes/cbn/img/sponsors/OnlineMBA.png" alt="WSU Online MBA Program" title="WSU Online MBA Program"></a>
 			<a href="https://www.libertymutual.com/wsu-alumni"><img src="/wp-content/themes/cbn/img/sponsors/LibertyMutual.png" alt="liberty mutual" title="liberty mutual"></a>
-		</aside><!--#right-col-->
+		</aside>--><!--#right-col-->
 	<?php }  // This is a homepage  ?>
 </div><!-- #primary -->
     
