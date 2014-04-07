a9f0680a9c67903ecd515a021a738f1cc6cfe65b
jeremyBass <jeremybass@cableone.net>
2014-04-07 09:59:32 -0700

    adds country to the require list

--- wp-content/themes/cbn2014/js/ini.js
+++ wp-content/themes/cbn2014/js/ini.js
@@ -372,6 +372,8 @@ jQuery(document).ready(function(){
 		jQuery('input[id^="cn-address"][name$="[city]"]').attr('required','required');
 		jQuery('input[id^="cn-address"][name$="[state]"]').attr('required','required');
 		jQuery('input[id^="cn-address"][name$="[zipcode]"]').attr('required','required');
+		jQuery('input[id^="cn-address"][name$="[country]"]').attr('required','required');
+		
 		
 		jQuery('[name="contact_first_name"]').attr('required','required');
 		jQuery('[name="contact_last_name"]').attr('required','required');
